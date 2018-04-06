0f75ef27fb6545e711b5cb155f946dfb3bb12078
Benny Nguyen <benny.nguyenx@gmail.com>
2018-04-06 14:41:16 -0700

    Complete philosopher tests

--- main.kts
+++ main.kts
@@ -57,7 +57,19 @@ val r2 = process("FOO",
 ) 
 
 // write an enum-based state machine between talking and thinking
-enum class Philosopher { }
+enum class Philosopher {
+    THINKING {
+        override fun signal() = TALKING
+        override fun toString() = "Deep thoughts...."
+    },
+
+    TALKING {
+        override fun signal() = THINKING
+        override fun toString() = "Allow me to suggest an idea..."
+    };
+    
+    abstract fun signal(): Philosopher
+}
 
 // create an class "Command" that can be used as a function (provide an "invoke()" function)
 // that takes a single parameter ("message" of type String)
@@ -75,7 +87,7 @@ println("map fold test: " + if (mapFoldResults == "FIZZBUZZFIZZFIZZBUZZFIZZFIZZB
 println("r1 test: " + if (r1 == ">>> FOO: {BAR}") "." else "!")
  
 println("r2 test: " + if (r2 == ">>> FOO: {WOOGAWOOGAWOOGA}") "." else "!")
-/*
+
 var seneca = Philosopher.THINKING
 print("Seneca, talk! ")
 seneca = seneca.signal()
@@ -86,7 +98,7 @@ println(if (seneca.toString() == "Deep thoughts....") "." else "!")
 print("Seneca, talk! ")
 seneca = seneca.signal()
 println(if (seneca.toString() == "Allow me to suggest an idea...") "." else "!")
-
+/* 
 print("Command tests: ")
 print(if (Command("")("") == "") "." else "!")
 print(if (Command("> ")("Hello!") == "> Hello!") "." else "!")
